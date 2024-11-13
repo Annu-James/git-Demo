93525067d834babbf67e0fbca9e932e33a635a12
Annu-James <annamaria1551@gmail.com>
2024-11-13 18:44:01 +0530

    new change

--- NameEx.java
+++ NameEx.java
@@ -4,7 +4,7 @@ public class NameEx {
 public void add(int a,int b)
 {
 int c=a+b;	
-System.out.println(c);
+System.out.println("Sample: "+c);
 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
